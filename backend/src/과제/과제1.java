40a68ca598e8eb0610b82969067ab9465c4bf61a
504 <504@DESKTOP-GR47Q4P>
2023-07-13 17:32:53 +0900

    7월 13일 17시 30분 과제 커밋

---
+++ backend/src/과제/과제1.java
@@ -0,0 +1,36 @@
+package 과제;
+
+public class 과제1 {
+
+/*	1번 4
+	2번 6 7
+	3번 byte , boolean / short ,char / int , float / long , double
+	4번 int , double / age , price / 10 , 3.14
+	5번 3
+	6번 1
+	7번 4
+	8번 3
+	9번 3
+	10번 2
+	11번 13 , 16
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+*/	
+}
+
+
+
+// 1. 연산자 암기 확인 2인 1조
+// 2. 1인 책 73 ~ 76 풀이 // 카톡방에 깃 주소 올리기
