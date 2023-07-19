17d5e7316d76760b3e7f1e206724d930fde403ab
504 <504@DESKTOP-GR47Q4P>
2023-07-19 16:05:09 +0900

    .

--- backend/src/java1/day05/Ex2_문자열.java
+++ backend/src/java1/day05/Ex2_문자열.java
@@ -46,11 +46,40 @@ public class Ex2_문자열 {
 			System.out.println("여자 입니다");
 		}
 	
-		// p158
+		// p158 .length() 문자열의 길이 알기
 		System.out.println("주민등록번호길이 : " + ssn.length());
 	
-	
-	
+		// p159 . replace() 문자열내 특정 문자열 교체/치환/대체		
+		String oldStr = "자바 문자열은 불변입니다. 자바 문자열은 String입니다.";
+		// 문자열내 "자바" --> "java" 로 교체
+		String newStr = oldStr.replace("자바", "java");
+		System.out.println(oldStr);
+		System.out.println(newStr);
+		
+		// p160 .substring()	: 문자열 자르기
+		
+		String ssn2 = "880815-1234567";
+		String ssn3= ssn2.substring(7); // 해당 인덱스 이후로 전부 출력 // 앞은 생략
+		System.out.println(ssn3);
+		String ssn4 = ssn2.substring(0 , 6); // 시작인덱스 부터 마지막 인덱스의 전 까지 출력 // 0 1 2 3 4 5  6개 출력됨
+		System.out.println(ssn4);
+
+		// p164 .split()		: 문자열 분리
+			// ssn2.split("-") : - 기준으로 분리
+		
+		System.out.println(ssn2.split("-")[1]);
+		
+		// p162 .indexOf()	: 문자 찾기
+		
+		String subject = "자바프로그래밍";
+		int location = subject.indexOf("프로그래밍");
+		if (location == -1) {
+			System.out.println("못찾음");
+		}else {
+			System.out.println("찾음");
+		}
+		
+		System.out.println(location);
 		
 		
 	}//main
@@ -66,18 +95,13 @@ public class Ex2_문자열 {
 	자바 문자열에서 제공해주는 라이브러리 = 미리 만들어진 함수
 		문자열객채.메소드명()
 		1. 문자열객체1.equals(문자열객체2) // 문자열 그 자체를 비교 true || false
-
-
-
-
-
-
-
-
-
-
-
-
+		2. 문자열객체.charAt(인덱스)
+		3. 문자열객체.length()
+			vs 배열명.length
+		4. 문자열객체.replace("기존문자열", "새로운문자열") 문자열내 "기존문자열" 이라는 문자열이 존재하면 "새로운문자열" 로 치환해서 반환
+		5.
+			문자열객체.split("분리기준문자") 
+		6. 문자열.indexOf("찾고싶은문자열") // 문자열내 찾는 문자열이 존재하면 찾은 index를 반환 / 없으면 -1 반환
 
 
 
