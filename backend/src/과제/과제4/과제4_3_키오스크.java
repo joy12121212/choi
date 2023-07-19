92dc999864f1e7d36d9605a94ee0d1c246e6f873
504 <504@DESKTOP-GR47Q4P>
2023-07-19 11:42:55 +0900

    과제4 완료

--- backend/src/과제/과제4/과제4_3_키오스크.java
+++ backend/src/과제/과제4/과제4_3_키오스크.java
@@ -13,6 +13,8 @@ public class 과제4_3_키오스크 {
 		int 콜라바구니 = 0 ; 	int 환타바구니 = 0 ; 	int 사이다바구니 = 0;
 		
 		int 콜라가격 = 300 ;	int 환타가격 = 200 ;	int 사이다가격 = 100 ; 
+
+		int 총금액 = 0;//변수 추가
 		
 		while(true) {
 			System.out.println("\n\n-------------------- 메뉴 ------------------- ");
@@ -20,7 +22,75 @@ public class 과제4_3_키오스크 {
 			System.out.print(">>>>>> 선택 : "); int ch = scanner.nextInt();
 			
 			/* 문제풀이 위치 */
+
 			
+			System.out.println("제품명\t"+"수량\t"+"가격");
+			
+			if (ch == 1) {
+				if (콜라재고 == 0) {
+					System.out.println("재고부족");
+				}else {
+					콜라바구니++;
+					System.out.println("콜라\t" + 콜라바구니+"\t" + (콜라가격*콜라바구니));
+					총금액 += 콜라가격;
+					System.out.println(총금액);
+					콜라재고--;
+
+				}
+
+
+				
+			}else if (ch == 3) {
+				if (사이다재고 == 0) {
+					System.out.println("재고부족");
+				}else {
+					사이다바구니++;
+					System.out.println("사이다\t" + 사이다바구니+"\t" + (사이다가격*사이다바구니));
+					총금액 += 사이다가격;
+					System.out.println(총금액);
+					사이다재고--;
+
+				}
+			}else if (ch == 2) {
+				if (환타재고 == 0) {
+					System.out.println("재고부족");
+				}else {
+					환타바구니++;
+					System.out.println("환타\t" + 환타바구니+"\t" + (환타가격*환타바구니));
+					총금액 += 환타가격;
+					System.out.println(총금액);
+					환타재고--;
+
+				}
+			}else if(ch == 4) {
+
+				System.out.println("콜라\t" + 콜라바구니+"\t" + (콜라가격*콜라바구니));
+				System.out.println("사이다\t" + 사이다바구니+"\t" + (사이다가격*사이다바구니));
+				System.out.println("환타\t" + 환타바구니+"\t" + (환타가격*환타바구니));
+				System.out.println("총 가격 : " + ((콜라가격*콜라바구니) + (사이다가격*사이다바구니)+(환타가격*환타바구니)));
+				
+				System.out.println("1. 결제 2. 취소");
+				int 결제 = scanner.nextInt();
+
+				
+				if (결제 == 1) {
+					System.out.println("결제 금액을 입력 하세요");
+					int 금액 = scanner.nextInt();
+
+					if (금액 < 총금액) {
+						System.out.println("결제 취소");
+						환타바구니=0; 콜라바구니=0; 사이다바구니=0; 콜라재고=10; 환타재고=10; 사이다재고=10; 총금액=0;
+					}else{
+						System.out.println("결제 완료");
+						환타바구니=0; 콜라바구니=0; 사이다바구니=0; 콜라재고=10; 환타재고=10; 사이다재고=10; 총금액=0;
+					}
+				}else if (결제 == 2){
+					System.out.println("취소 되었습니다.");
+					환타바구니=0; 콜라바구니=0; 사이다바구니=0; 콜라재고=10; 환타재고=10; 사이다재고=10; 총금액=0;
+				}else {
+					System.out.println("다시 입력 하세요");
+				}
+			}
 			
 			/* ----------- */
 			
