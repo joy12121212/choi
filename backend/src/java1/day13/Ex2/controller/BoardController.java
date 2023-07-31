4ffa85981519027cd83ca0fc8e9dca27fb2891d4
504 <504@DESKTOP-G2B2I5O>
2023-07-31 11:34:42 +0900

    1

---
+++ backend/src/java1/day13/Ex2/controller/BoardController.java
@@ -0,0 +1,15 @@
+package java1.day13.Ex2.controller;
+
+public class BoardController {
+	
+	// 싱글톤 : 게시물마다가 아닌 모든 게시물이 하나의 객체를 통해 동일한 기능 수행
+	
+	// 글쓰기 처리 기능 함수
+	
+	// 글출력 처리 기능 함수
+	
+	// 글삭제 처리 기능 함수
+	
+	// 글수정 처리 기능 함수
+
+}
---
+++ backend/src/java1/day13/Ex2/model/dto/BoardDto.java
@@ -0,0 +1,5 @@
+package java1.day13.Ex2.model.dto;
+
+public class BoardDto {
+	//싱글톤X : 게시물1개 당 DTO 1개 이기 때문에
+}
