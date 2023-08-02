ad04b83d8b93fa1e0cf6922e90512c7b2eba1acf
504 <504@DESKTOP-G2B2I5O>
2023-08-02 17:16:45 +0900

    1

--- backend/src/java1/day15/Ex2_자동타입변환/Child.java
+++ backend/src/java1/day15/Ex2_자동타입변환/Child.java
@@ -1,5 +1,17 @@
 package java1.day15.Ex2_자동타입변환;
 
-public class Child {
-
+public class Child extends Parent{
+			// child extends Parent -> 상속
+	
+	@Override
+	public void method1() {
+		System.out.println("자식클래스 메소드1");
+	}
+	
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		super.method2();
+	}
+	
 }
